706104d7f4a8f99a21f2eca41aed7a2c1b34d737
Abrar Rahman Protyasha <a_protyasha@apple.com>
2023-12-01 03:49:27 -0800

    Initial Commit

---
+++ AdventOfCode2023/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AdventOfCode2023
+//
+//  Created by Abrar Rahman Protyasha on 12/1/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
