f37e7fa6ba684f57e60e654aee80431c2a8d30c8
Abrar Rahman Protyasha <a_protyasha@apple.com>
2023-12-03 03:23:17 -0800

    Use TCLAP for CLI argument parsing solution
    This commit does a couple of things:
    1. Vendors the TCLAP library and hooks up the Xcode search path to look
       for it.
    2. Migrates current barebones argument parsing solution to adopt TCLAP
       instead.
    3. Drive-by fixes in day 1 problem solver.
    
    Solves issue #1. Argument parsing should be breezy now.
    
    Signed-off-by: Abrar Rahman Protyasha <abrar.rahman98@gmail.com>

--- AdventOfCode2023/day1.cpp
+++ AdventOfCode2023/day1.cpp
@@ -15,7 +15,7 @@
 
 namespace Day1 {
 
-void solvePart1(const Utilities::ProblemInput& input)
+void solvePart1(const Utilities::ProblemInput& input, Utilities::ShowResults showResults)
 {
     uint64_t sumOfCalibrationValues = 0;
     std::ranges::for_each(input.cbegin(), input.cend(), [&sumOfCalibrationValues](const auto& line) {
@@ -26,14 +26,22 @@ void solvePart1(const Utilities::ProblemInput& input)
         calibrationValue += static_cast<uint64_t>(*std::ranges::find_if(line.crbegin(), line.crend(), isDigit) - '0');
         sumOfCalibrationValues += calibrationValue;
     });
-    std::cout << "Day 1 part 1 solution: " << sumOfCalibrationValues << '\n';
+
+    if (showResults == Utilities::ShowResults::Yes)
+        std::cout << "Day 1 part 1 solution: " << sumOfCalibrationValues << '\n';
 }
 
-void solveProblem(const Utilities::ProblemInputs& inputs, Utilities::ShowResults)
+void solvePart2(const Utilities::ProblemInput& input, Utilities::ShowResults showResults)
 {
-    const auto& [input1, _] = inputs;
 
-    solvePart1(input1);
+}
+
+void solveProblem(const Utilities::ProblemInputs& inputs, Utilities::ShowResults showResults)
+{
+    const auto& [input1, input2] = inputs;
+
+    solvePart1(input1, showResults);
+    solvePart2(input2, showResults);
 }
 
 } // namespace Day1
